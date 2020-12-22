5e415851d8355cd7955a31369c42845eb484d5e2
llanos <llanos1205@gmail.com>
2020-12-21 20:40:19 -0400

    router added

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,15 @@
+<template>
+ <search-bar>
+
+ </search-bar>
+</template>
+
+<script>
+export default {
+name: "SearchBar"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Donations.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="donation">
+    <h1>This is an donation page</h1>
+  </div>
+</template>
